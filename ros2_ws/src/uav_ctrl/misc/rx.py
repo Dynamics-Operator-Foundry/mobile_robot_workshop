fca25cdac511ec1c1e07a4bb556cc0170332ef12
pattylo <19451945as@gmail.com>
2026-02-03 16:26:14 -0500

    lala commit

--- ros2_ws/src/uav_ctrl/misc/rx.py
+++
@@ -1,6 +0,0 @@
-import socket, json
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("0.0.0.0", 5005))
-while True:
-    data, addr = sock.recvfrom(1024)
-    print(addr, json.loads(data))
--- vicon/vrpn.sh
+++
@@ -1,1 +0,0 @@
-ros2 launch client.launch.yaml server:=192.168.0.101 port:=3883
